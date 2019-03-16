c7115d38217a6094fbc274122e731dc3e1f86ba3
Will Jones <willjones2010@gmail.com>
2019-03-16 18:00:13 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -44,8 +44,7 @@ end
 
 def for_iterator(number_of_times)
   for 1..number_of_times
-  
-  phrase = "Welcome to Flatiron School's Web Development Course!"
-  
+  puts "Welcome to Flatiron School's Web Development Course!"
+end
 end
 
