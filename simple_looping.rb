29d14cd637640372ca8dac33f892e165352b5fb2
Will Jones <willjones2010@gmail.com>
2019-03-16 17:57:56 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -37,7 +37,7 @@ end
 def until_iterator(number_of_times)
   counter = 0 
   until counter = number_of_times
-  counter =+ 1
+  counter = counter + 1
   puts "Welcome to Flatiron School's Web Development Course!"
 end
 end
