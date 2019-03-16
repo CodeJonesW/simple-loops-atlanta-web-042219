55a8e6dd534ceedeabef6a038de19f1ed84cc6a9
Will Jones <willjones2010@gmail.com>
2019-03-16 18:06:42 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -43,7 +43,7 @@ end
 end
 
 def for_iterator(number_of_times)
-  for number_of_times in 1..number_of_times do
+  for counter in 1..number_of_times do
   puts "Welcome to Flatiron School's Web Development Course!"
 end
 
